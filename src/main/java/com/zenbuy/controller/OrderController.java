3fdf0ed60a73f86a6463f6d6be0a86859f60ea9b
Yajvin Pulavarthi <23f1001586@ds.study.iitm.ac.in>
2025-12-30 19:53:51 +0530

    Update CORS policy to allow all origins

--- src/main/java/com/zenbuy/controller/OrderController.java
+++ src/main/java/com/zenbuy/controller/OrderController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/orders")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class OrderController {
 
     private final OrderService orderService;
