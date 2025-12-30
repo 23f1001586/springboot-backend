0dee4b82cc522eec181fb61b855f6fb0a3176603
Yajvin Pulavarthi <23f1001586@ds.study.iitm.ac.in>
2025-12-30 19:52:37 +0530

    Change CORS policy to allow all origins

--- src/main/java/com/zenbuy/controller/AuthController.java
+++ src/main/java/com/zenbuy/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final UserService userService;
