40ec89f938083f5738a13bd174efcd77d5937c8f
Yajvin Pulavarthi <23f1001586@ds.study.iitm.ac.in>
2025-12-30 19:53:28 +0530

    Allow CORS from all origins in OAuth2Controller

--- src/main/java/com/zenbuy/controller/OAuth2Controller.java
+++ src/main/java/com/zenbuy/controller/OAuth2Controller.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/oauth2")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class OAuth2Controller {
 
     private final UserRepository userRepository;
