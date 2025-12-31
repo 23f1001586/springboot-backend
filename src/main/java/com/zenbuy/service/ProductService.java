166ff5aa3eb19bd5c12b5d3b33ac5fc92ab9227d
Yajvin Pulavarthi <23f1001586@ds.study.iitm.ac.in>
2025-12-31 10:28:55 +0530

    Add Profile annotation to ProductService

--- src/main/java/com/zenbuy/service/ProductService.java
+++ src/main/java/com/zenbuy/service/ProductService.java
@@ -3,10 +3,12 @@ package com.zenbuy.service;
 import com.zenbuy.model.Product;
 import com.zenbuy.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
 @Service
+@Profile("dev")
 public class ProductService {
 
     private final ProductRepository repo;
